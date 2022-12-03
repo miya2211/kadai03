e933f55d87ab2af97fcc914b9c3b83ea862a9547
MiyanoAmi <a08018656523@icloud.com>
2022-12-03 14:02:14 +0900

    ・変数名inventoryをinventoryMapに変更しました。 ・IDE 設定を開き、ツール | 保存時のアクションを選択し、コードの整形オプションを有効にしました。 ・isNullを使用しました。

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,38 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
         //在庫数Map（ 品名 , 個数 ）
-        Map<String, Integer> inventory = new HashMap<>();
-        inventory.put("箱ティッシュ", 6);
-        inventory.put("トイレットペーパー", 4);
-        inventory.put("ラップ", 2);
-        inventory.put("洗濯用洗剤", 1);
+        Map<String, Integer> inventoryMap = new HashMap<>();
+        inventoryMap.put("箱ティッシュ", 6);
+        inventoryMap.put("トイレットペーパー", 4);
+        inventoryMap.put("ラップ", 2);
+        inventoryMap.put("洗濯用洗剤", 1);
 
         //在庫数 （ 品名：個数 ）
         System.out.println("【在庫数】");
-        inventory.forEach((goods, stock) -> System.out.println(goods + "：" + stock));
+        inventoryMap.forEach((goods, stock) -> System.out.println(goods + "：" + stock));
 
         System.out.println(); //改行
 
         //例外処理
         System.out.println("【例外処理】");
         try {
-            if (null != inventory.get("ざる")) {
-                System.out.println(inventory.get("ざる"));
+            if (null != inventoryMap.get("ざる")) {
+                System.out.println(inventoryMap.get("ざる"));
             } else {
                 throw new NullPointerException();
             }
 
         } catch (NullPointerException e) {
+            System.out.println("データはありません");
+        }
 
+        //isNull使用
+        if (Objects.isNull(inventoryMap.get("パン"))) {
             System.out.println("データはありません");
         }
     }
